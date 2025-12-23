31172cde05851687463cfe4fc59629b7905c0b9f
Causland <christopher.auslander@gmail.com>
2025-12-22 20:51:48 -0500

    Force nvim-treesitter to v0.10.0. Latests main breaks nvim load

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,7 @@
 return {
   -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
+  tag = "v0.10.0",
   dependencies = {
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
